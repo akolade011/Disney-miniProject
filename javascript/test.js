e89c362deb699c9fd9503e17a47c1dab5e64ef71
Hassan Akinwande <akolakoladeakinwande@hotmail.com>
2021-05-26 15:58:45 +0100

    Disney homepage

---
+++ javascript/test.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.heroImage').slick({
+        autoplay: true,
+        fade: true,
+        dots:true
+       
+    });
+  });
+          
+
+
+
+
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true
+
+  });
